19f189390d5145ec703d1842915b6a1b6df27175
Alexandr Yermak <ayermak@protonmail.ch>
2024-01-04 19:29:55 +0300

    fix func signatures

--- plugins/function-signatures.lua
+++ plugins/function-signatures.lua
@@ -4,6 +4,8 @@ return {
     "ray-x/lsp_signature.nvim",
     opts = {
       hint_enable = false, -- disable hints as it will crash in some terminal
+      floating_window_above_cur_line = false,
+      floating_window_off_y = -2,
     },
   },
 }
