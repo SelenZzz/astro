34f9645208a0ccd5f8e989162f2602eff2fe5b3a
Alexandr Yermak <ayermak@protonmail.ch>
2024-01-06 21:59:42 +0300

    focus files only on command

--- plugins/neo-tree.lua
+++ plugins/neo-tree.lua
@@ -8,7 +8,17 @@ return {
         visible = true,
         hide_dotfiles = true,
         hide_gitignored = true,
-      }
-    }
+      },
+      follow_current_file = {
+        enabled = false,
+      },
+    },
+  },
+  keys = {
+    {
+      "<leader>bf",
+      "<Cmd>:Neotree focus reveal_force_cwd<CR>",
+      desc = "Focus explorer location",
+    },
   },
 }
