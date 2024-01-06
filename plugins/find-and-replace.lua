e937909c81edea7bb90a583589288b0b19d8cdc2
Alexandr Yermak <ayermak@protonmail.ch>
2024-01-06 16:38:17 +0300

    add find and resplace

--- plugins/find-and-replace.lua
+++ plugins/find-and-replace.lua
@@ -2,6 +2,13 @@ return {
   "ray-x/sad.nvim",
   dependencies = { "ray-x/guihua.lua", build = "cd lua/fzy && make" },
   opts = {},
-  event = "User AstroFile",
-  cmd = { "Sad" },
+  -- event = "User AstroFile",
+  -- cmd = { "Find and Replace" },
+  keys = {
+    {
+      "<leader>fR",
+      "<Cmd>Sad<CR>",
+      desc = "Find and replace",
+    },
+  },
 }
